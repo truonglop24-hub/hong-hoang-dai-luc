976768cb4935a9059048919142f88d9bfd4194d0
truonglop24-hub <truonglop24@gmail.com>
2026-08-10 20:32:47 +0700

    Create batdau.js

---
+++ batdau.js
@@ -0,0 +1,51 @@
+const {
+    SlashCommandBuilder,
+    EmbedBuilder
+} = require("discord.js");
+
+const {
+    createPlayer
+} = require("../../database/database");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("batdau")
+        .setDescription("Bắt đầu con đường tu tiên"),
+
+    async execute(interaction) {
+
+        const player = createPlayer(
+            interaction.user.id,
+            interaction.user.username
+        );
+
+        if (player.created_at) {
+            const embed = new EmbedBuilder()
+                .setTitle("🌌 Hồng Hoang Khai Thiên")
+                .setDescription(
+                    `Đạo hữu **${interaction.user.username}** đã bước vào con đường tu tiên!`
+                )
+                .addFields(
+                    {
+                        name: "🌿 Linh căn",
+                        value: player.spirit_root,
+                        inline: true
+                    },
+                    {
+                        name: "⭐ Thiên phú",
+                        value: player.talent,
+                        inline: true
+                    },
+                    {
+                        name: "🌀 Cảnh giới",
+                        value: player.realm_name,
+                        inline: true
+                    }
+                );
+
+            return interaction.reply({
+                embeds: [embed]
+            });
+        }
+    }
+};
