db85b2005d6cd7e21bf9ee749805bf355210836f
truonglop24-hub <truonglop24@gmail.com>
2026-08-10 20:31:14 +0700

    Create realms.js

---
+++ realms.js
@@ -0,0 +1,94 @@
+const realms = [
+    {
+        id: 0,
+        name: "Phàm Nhân",
+        maxCultivation: 1000
+    },
+    {
+        id: 1,
+        name: "Luyện Khí",
+        maxCultivation: 10000
+    },
+    {
+        id: 2,
+        name: "Trúc Cơ",
+        maxCultivation: 30000
+    },
+    {
+        id: 3,
+        name: "Kim Đan",
+        maxCultivation: 80000
+    },
+    {
+        id: 4,
+        name: "Nguyên Anh",
+        maxCultivation: 200000
+    },
+    {
+        id: 5,
+        name: "Hóa Thần",
+        maxCultivation: 500000
+    },
+    {
+        id: 6,
+        name: "Luyện Hư",
+        maxCultivation: 1000000
+    },
+    {
+        id: 7,
+        name: "Hợp Thể",
+        maxCultivation: 3000000
+    },
+    {
+        id: 8,
+        name: "Đại Thừa",
+        maxCultivation: 10000000
+    },
+    {
+        id: 9,
+        name: "Độ Kiếp",
+        maxCultivation: 30000000
+    },
+    {
+        id: 10,
+        name: "Chân Tiên",
+        maxCultivation: 100000000
+    },
+    {
+        id: 11,
+        name: "Kim Tiên",
+        maxCultivation: 500000000
+    },
+    {
+        id: 12,
+        name: "Thái Ất Kim Tiên",
+        maxCultivation: 2000000000
+    },
+    {
+        id: 13,
+        name: "Đại La Kim Tiên",
+        maxCultivation: 10000000000
+    },
+    {
+        id: 14,
+        name: "Chuẩn Thánh",
+        maxCultivation: 50000000000
+    },
+    {
+        id: 15,
+        name: "Thánh Nhân",
+        maxCultivation: 100000000000
+    },
+    {
+        id: 16,
+        name: "Thiên Đạo",
+        maxCultivation: 1000000000000
+    },
+    {
+        id: 17,
+        name: "Đại Đạo",
+        maxCultivation: 9999999999999
+    }
+];
+
+module.exports = realms;
